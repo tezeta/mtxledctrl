2a7804bca91c544acaf2a73ba2895d5d3cb32551
tezeta <25644493+tezeta@users.noreply.github.com>
2021-11-02 02:38:12 -0400

    overhaul and split files

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# ledproc
-LED controller for LCDs through LCDd (specifically MtxOrb). 
-Currently, this only monitors a file (i.e. /proc/diskstats), intended for use with hard drive activity.
+# mtxledctrl
+LED controller for Matrix Orbital LCDs through LCDd. Tested with the LK204-7T-1U, should work with similiar models with LEDs.
+Each LED can be either red, orange or green. Color changes are queued before sending them:
+```
+>>> import mtxledctrl
+>>> tn = mtxledctrl.init("127.0.0.1","13666")
+>>> mtxledctrl.queuecolor(1,'r')
+>>> mtxledctrl.queuecolor(2,'o')
+>>> mtxledctrl.queuecolor(3,'g')
+>>> mtxledctrl.sendcmd(tn)
+```
+Includes a demo (ledproc), this only monitors a file for changes (i.e. /proc/diskstats), intended for use with hard drive activity.
---
+++ mtxledctrl.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+#ledproc 1.0
+#tezeta 2020
+
+import ctypes, time, telnetlib
+
+c_uint8 = ctypes.c_uint8
+debugOn = False
+
+#led status is controlled via bitmask
+#six GPO bits control the LEDS
+#more info in Matrix Orbital's documentation
+
+class GPIO_bits( ctypes.LittleEndianStructure ):
+    _fields_ = [
+                ("a", c_uint8, 1 ),
+                ("b", c_uint8, 1 ),
+                ("c", c_uint8, 1 ),
+                ("d", c_uint8, 1 ),
+                ("e", c_uint8, 1 ),
+                ("f", c_uint8, 1 ),
+               ]
+
+class GPIO( ctypes.Union ):
+    _anonymous_ = ("bit",)
+    _fields_ = [
+                ("bit",    GPIO_bits ),
+                ("asByte", c_uint8    )
+               ]
+
+gpio = GPIO()
+gpio.asByte = 0x0
+
+def debug(message):
+    if debugOn:
+        print("debug: " + str(message))
+    return message
+
+def error(message):
+    print("ERROR: " + str(message))
+    return message
+
+def init(ip, port):
+    try:
+        tn = debug(telnetlib.Telnet(ip, port))
+        tn.write(b"hello\n")
+        debug(tn.read_until(b'\n'))
+    except ConnectionRefusedError:
+        error("Failed to connect to server!")
+        exit(1)
+
+    for colortest in ['r','o','g','']:
+        for ledtest in ['1','2','3']:
+            debug("testing led " + ledtest)
+            queuecolor(ledtest, colortest)
+        sendcmd(tn)
+        time.sleep(0.5)
+
+    return tn
+
+def sendcmd(sock):
+    byte = str(gpio.asByte) # get bitmask as number
+    string = "output " + str(gpio.asByte) + "\n"
+    debug("send command \"" + string.rstrip() + "\"")
+    sock.write(string.encode('utf-8'))
+    debug("recieved \"" + str(sock.read_until(b"\n")) + "\"")
+
+def queuecolor(led, color):
+    #led logic based on MXO's documentation
+    #if the color is not defined assume it should be off
+    led = int(led)
+    if led==1:
+        gpio.a=0
+        gpio.b=0
+        if color=='r':
+            gpio.a=1
+            gpio.b=0
+        if color=='g':
+            gpio.a=0
+            gpio.b=1
+        if color=='o':
+            gpio.a=1
+            gpio.b=1
+    if led==2:
+        gpio.c=0
+        gpio.d=0
+        if color=='r':
+            gpio.c=1
+            gpio.d=0
+        if color=='g':
+            gpio.c=0
+            gpio.d=1
+        if color=='o':
+            gpio.c=1
+            gpio.d=1
+    if led==3:
+        gpio.e=0
+        gpio.f=0
+        if color=='r':
+            gpio.e=1
+            gpio.f=0
+        if color=='g':
+            gpio.e=0
+            gpio.f=1
+        if color=='o':
+            gpio.e=1
+            gpio.f=1
+
