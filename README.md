9f89ce109a1f2055c6e4e565f8ab465107b3411c
tezeta <25644493+tezeta@users.noreply.github.com>
2021-11-03 04:22:30 -0400

    streamline

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 LED controller for Matrix Orbital LCDs through LCDd. Tested with the LK204-7T-1U, should work with similiar models with LEDs.
 Each LED can be either red, orange or green. Color changes are queued before sending them:
 ```
->>> import mtxledctrl
->>> tn = mtxledctrl.init("127.0.0.1","13666")
->>> mtxledctrl.queuecolor(1,'r')
->>> mtxledctrl.queuecolor(2,'o')
->>> mtxledctrl.queuecolor(3,'g')
->>> mtxledctrl.sendcmd(tn)
+>>> from mtxledctrl import mtxledctrl
+>>> ledctrl = mtxledctrl("127.0.0.1","13666",True) #ip and port of LCDd server, debug messages
+>>> ledctrl.queuecolor(1,'r')
+>>> ledctrl.queuecolor(2,'o')
+>>> ledctrl.queuecolor(3,'g')
+>>> ledctrl.sendcmd()
 ```
-Includes a demo (ledproc), this only monitors a file for changes (i.e. /proc/diskstats), intended for use with hard drive activity.
+Includes a demo (ledproc), this monitors /proc/diskstats and flashes LEDs based on drive activity. Use IDs found in /dev/disk/by-id and seperate them with |. Useful for showing activity for specific RAID arrays.
--- mtxledctrl.py
+++ mtxledctrl.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-#mtxledctrl 1.0
+#mtxledctrl 1.1
 #tezeta 2021
 
 import ctypes, time, telnetlib
 
 c_uint8 = ctypes.c_uint8
-debugOn = False
 
 #led status is controlled via bitmask
 #six GPO bits control the LEDS
@@ -31,78 +30,78 @@ class GPIO( ctypes.Union ):
 gpio = GPIO()
 gpio.asByte = 0x0
 
-def debug(message):
-    if debugOn:
-        print("debug: " + str(message))
-    return message
+class mtxledctrl:
+    def __init__(self, ip, port, dbg):
+        self.dbg = dbg
 
-def error(message):
-    print("ERROR: " + str(message))
-    return message
+        try:
+            self.tn = self.debug(telnetlib.Telnet(ip, port))
+            self.tn.write(b"hello\n")
+            self.debug(self.tn.read_until(b'\n'))
+        except ConnectionRefusedError:
+            error("Failed to connect to server!")
+            exit(1)
 
-def init(ip, port):
-    try:
-        tn = debug(telnetlib.Telnet(ip, port))
-        tn.write(b"hello\n")
-        debug(tn.read_until(b'\n'))
-    except ConnectionRefusedError:
-        error("Failed to connect to server!")
-        exit(1)
+        for colortest in ['r','o','g','']:
+            for ledtest in ['1','2','3']:
+                self.debug("testing led " + ledtest)
+                self.queuecolor(ledtest, colortest)
+            self.sendcmd()
+            time.sleep(0.5)
 
-    for colortest in ['r','o','g','']:
-        for ledtest in ['1','2','3']:
-            debug("testing led " + ledtest)
-            queuecolor(ledtest, colortest)
-        sendcmd(tn)
-        time.sleep(0.5)
+    def debug(self, message):
+        if self.dbg:
+            print("debug: " + str(message))
+        return message
 
-    return tn
+    def error(self, message):
+        print("ERROR: " + str(message))
+        return message
 
-def sendcmd(sock):
-    byte = str(gpio.asByte) # get bitmask as number
-    string = "output " + str(gpio.asByte) + "\n"
-    debug("send command \"" + string.rstrip() + "\"")
-    sock.write(string.encode('utf-8'))
-    debug("recieved \"" + str(sock.read_until(b"\n")) + "\"")
+    def sendcmd(self):
+        byte = str(gpio.asByte) # get bitmask as number
+        string = "output " + str(gpio.asByte) + "\n"
+        self.debug("send command \"" + string.rstrip() + "\"")
+        self.tn.write(string.encode('utf-8'))
+        self.debug("recieved \"" + str(self.tn.read_until(b"\n")) + "\"")
 
-def queuecolor(led, color):
-    #led logic based on MXO's documentation
-    #if the color is not defined assume it should be off
-    led = int(led)
-    if led==1:
-        gpio.a=0
-        gpio.b=0
-        if color=='r':
-            gpio.a=1
-            gpio.b=0
-        if color=='g':
+    def queuecolor(self, led, color):
+        #led logic based on MXO's documentation
+        #if the color is not defined assume it should be off
+        led = int(led)
+        if led==1:
             gpio.a=0
-            gpio.b=1
-        if color=='o':
-            gpio.a=1
-            gpio.b=1
-    if led==2:
-        gpio.c=0
-        gpio.d=0
-        if color=='r':
-            gpio.c=1
-            gpio.d=0
-        if color=='g':
+            gpio.b=0
+            if color=='r':
+                gpio.a=1
+                gpio.b=0
+            if color=='g':
+                gpio.a=0
+                gpio.b=1
+            if color=='o':
+                gpio.a=1
+                gpio.b=1
+        if led==2:
             gpio.c=0
-            gpio.d=1
-        if color=='o':
-            gpio.c=1
-            gpio.d=1
-    if led==3:
-        gpio.e=0
-        gpio.f=0
-        if color=='r':
-            gpio.e=1
-            gpio.f=0
-        if color=='g':
+            gpio.d=0
+            if color=='r':
+                gpio.c=1
+                gpio.d=0
+            if color=='g':
+                gpio.c=0
+                gpio.d=1
+            if color=='o':
+                gpio.c=1
+                gpio.d=1
+        if led==3:
             gpio.e=0
-            gpio.f=1
-        if color=='o':
-            gpio.e=1
-            gpio.f=1
-
+            gpio.f=0
+            if color=='r':
+                gpio.e=1
+                gpio.f=0
+            if color=='g':
+                gpio.e=0
+                gpio.f=1
+            if color=='o':
+                gpio.e=1
+                gpio.f=1
